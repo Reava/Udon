ba7c4f64cf5143e3c2912c7d4e8f9ea7dc3f8e95
Reava <reava.vr@gmail.com>
2024-10-12 05:14:05 +0200

    Cleanup & Persistence implementation

--- Scripts/ObjectsToggle.cs
+++ Scripts/ObjectsToggle.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UdonSharp;
+using VRC.SDKBase;
+using VRC.SDK3.Persistence;
 
 namespace UwUtils
 {
@@ -9,13 +11,17 @@ namespace UwUtils
     {
         [Header("List of objects to toggle")]
         [SerializeField] private GameObject[] toggleObjects;
-        private bool valid = true;
+        [Header("Define a unique string for persistence, leave empty to not save.")]
+        [SerializeField] private string persistenceString;
+        private bool isToggled = false;
+
+        private bool shouldPersist = false;
+
         void Start()
         {
             if (toggleObjects.Length == 0)
             {
-                valid = false;
-                Debug.LogError("[UwUtils/iState.cs] Setup is invalid, check your references for object '" + gameObject.name + "'");
+                Debug.LogError("[UwUtils/iState.cs] No objects defined to toggle on '" + gameObject.name + "', did you mean this?");
             }
             else
             {
@@ -23,28 +29,24 @@ namespace UwUtils
             }
         }
 
-        public override void Interact()
+        public override void OnPlayerRestored(VRCPlayerApi player)
         {
-            foreach (GameObject toggleObject in toggleObjects)
+            if (string.IsNullOrWhiteSpace(persistenceString)) return;
+            if (!player.isLocal) return;
+            shouldPersist = true; // This is only true if persistence is available & used, wont be needed in the future.
+            if (PlayerData.TryGetBool(player, persistenceString, out bool objectState))
             {
-                toggleObject.SetActive(!toggleObject.activeSelf);
+                if (isToggled != objectState)
+                {
+                    isToggled = objectState;
+                    _InvertState(); // We only call this when the state was inverted before instead of loading the actual state of objects to make it simpler
+                }
             }
         }
 
-        public void _EnableAll()
-        {
-            foreach (GameObject toggleObject in toggleObjects)
-            {
-                toggleObject.SetActive(true);
-            }
-        }
-        public void _DisableAll()
-        {
-            foreach (GameObject toggleObject in toggleObjects)
-            {
-                toggleObject.SetActive(false);
-            }
-        }
+        public override void Interact() => _InvertState();
+
+        public void _SaveState() => PlayerData.SetBool(persistenceString, isToggled); // Save new state
 
         public void _InvertState()
         {
@@ -52,6 +54,28 @@ namespace UwUtils
             {
                 toggleObject.SetActive(!toggleObject.activeSelf);
             }
+            isToggled = !isToggled;
+            if (shouldPersist) _SaveState();
         }
+
+        /* Warning: This will cause persistence saving to be out of sync and is only meant as a code template!
+        public void _EnableAll()
+        {
+            foreach (GameObject toggleObject in toggleObjects)
+            {
+                toggleObject.SetActive(true);
+            }
+        } 
+        */ // Uncomment if you want to use it
+
+        /* Warning: This will cause persistence saving to be out of sync and is only meant as a code template!
+        public void _DisableAll()
+        {
+            foreach (GameObject toggleObject in toggleObjects)
+            {
+                toggleObject.SetActive(false);
+            }
+        }
+        */ // Uncomment if you want to use it
     }
 }
